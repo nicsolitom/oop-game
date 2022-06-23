71feecfe490de24bd149b19b3bee41fcb9a794d6
Nic Solitom <nicsolitom@gmail.com>
2022-06-23 17:20:30 +0200

    create and move obstacles

--- js/main.js
+++ js/main.js
@@ -1,12 +1,40 @@
 class Game {
   constructor() {
+    this.time = 0;
     this.player = null; // will store an instance of the class Player
+    this.obstaclesArr = []; // will store instances of the class Obstacles
   }
   start() {
-    console.log("starting game...");
-
     this.player = new Player();
     this.attachEventListeners();
+
+    // setInterval(() => {
+    //     const newObstacle = new Obstacle();
+    //     this.obstaclesArr.push(newObstacle);
+    // }, 3000);
+
+    setInterval(() => {
+        this.obstaclesArr.forEach((obstacle) => {
+            let currentVh = obstacle.domElement.style.bottom;
+            currentVh = Number.parseInt(currentVh.slice(0, 2));
+            currentVh -= 1;
+            obstacle.domElement.style.bottom = currentVh + "vh";
+        });
+        if (this.time % 60 === 0) {
+            const newObstacle = new Obstacle();
+            this.obstaclesArr.push(newObstacle);
+        }
+        this.time++;
+    }, 50);
+
+        // // move obstacles
+        // setInterval(() => {
+        //     this.obstacleArr.forEach((obstacleInstance) => {
+        //         obstacleInstance.moveDown();
+        //     });
+
+        // }, 50);
+      
   }
   attachEventListeners() {
     document.addEventListener("keydown", (event) => {
@@ -41,7 +69,7 @@ class Player {
     this.domElement.style.position = "absolute";
     this.domElement.style.left = this.positionX + "vw";
     this.domElement.style.bottom = this.positionY + "vh";
-    
+
     const boardElm = document.getElementById("board"); // to improve
     boardElm.appendChild(this.domElement);
   }
@@ -49,8 +77,8 @@ class Player {
     this.positionX++;
     this.domElement.style.left = this.positionX + "vw";
     console.log(this.positionX);
-}
-moveLeft() {
+  }
+  moveLeft() {
     this.positionX--;
     this.domElement.style.left = this.positionX + "vw";
     console.log(this.positionX);
@@ -59,6 +87,31 @@ moveLeft() {
     console.log(this.positionX);
   }
 }
+
+class Obstacle {
+    constructor() {
+        this.positionX = 45;
+        this.positionY = 90;
+        this.domElement = null;
+        this.createDomElement();
+      }
+      createDomElement() {
+        this.domElement = document.createElement("div");
+        this.domElement.className = "obstacle";
+    
+        // this.domElement.style.position = "absolute";
+        this.domElement.style.left = this.positionX + "vw";
+        this.domElement.style.bottom = this.positionY + "vh";
+    
+        const boardElm = document.getElementById("board"); // to improve
+        boardElm.appendChild(this.domElement);
+      }
+    //   moveDown(){
+    //     this.positionY--;
+    //     this.domElement.style.bottom = this.positionY + "vh";
+    // }
+}
+
 
 const game = new Game();
 game.start();
